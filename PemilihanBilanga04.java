c8720cff2ccc3b445712bd01b4b235316225178e
KemiiZkaria <ahmadkevinmalik01@gmail.com>
2024-09-26 07:43:13 +0700

    Melakukan perubahan pada file PemilihanBilangan dengan menggunakan ternary operator, dan melakukan upload file PemilihanHari

--- PemilihanBilanga04.java
+++ PemilihanBilanga04.java
@@ -5,14 +5,19 @@ public class PemilihanBilanga04 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        String hasil;
+
         System.out.print("Masukkan sebuah angka: ");
         int angka = sc.nextInt();
 
-        if (angka % 2 == 0) {
-            System.out.println("Angka " + angka + " termasuk bilangan genap");
-        }
-        else {
-            System.out.println("Angka " + angka + " termasuk bilangan ganjil");
-        }
+        // if (angka % 2 == 0) {
+        //     System.out.println("Angka " + angka + " termasuk bilangan genap");
+        // }
+        // else {
+        //     System.out.println("Angka " + angka + " termasuk bilangan ganjil");
+        // }
+
+        hasil = (angka % 2 == 0) ? ("Angka " + angka + " termasuk bilangan genap") : ("Angka " + angka + " termasuk bilangan ganjil");
+        System.out.println(hasil);
     }
 }
